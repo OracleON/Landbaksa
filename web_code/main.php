78c74f50a1c68ca4d2b1bf17d599f0a42e65c702
LEE SANG ON <LeeSangOn@LEEui-MacBook-Pro.local>
2017-07-20 13:36:43 +0900

    first setUP

---
+++ web_code/main.php
@@ -0,0 +1,16 @@
+<HTML>
+<head>
+	<? include $_SERVER["DOCUMENT_ROOT"]."/landbaksa/database/inc_dbconnect.php"; ?>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+</head>
+
+<body bgcolor="#f0f0f0">
+	<div id="wrap">
+		땅꾼박사 프로젝트 시작~~!!
+	</div>	
+	
+</body>	
+</HTML>
