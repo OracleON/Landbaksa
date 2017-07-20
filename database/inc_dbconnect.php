6413ed3255e749dc2675aa333813ea3435b9d065
LEE SANG ON <LeeSangOn@LEEui-MacBook-Pro.local>
2017-07-20 13:38:11 +0900

    1.1.1

--- database/inc_dbconnect.php
+++ database/inc_dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-	mysql_connect('localhost','pianoontest','on73057080')
+	mysql_connect('localhost','pianoontest','******')
 	or die("Unable to connect to SQL server");
 	mysql_select_db("pianoontest") or die("Unable to select database");
 ?>
