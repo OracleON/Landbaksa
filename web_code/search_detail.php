939f4b450644781a335c3acc2f81d067713ab06f
LEE SANG ON <LeeSangOn@LEEui-MacBook-Pro.local>
2017-07-23 02:12:16 +0900

    7-23-1

---
+++ web_code/search_detail.php
@@ -0,0 +1,45 @@
+<HTML>
+<head>
+	<? include $_SERVER["DOCUMENT_ROOT"]."/landbaksa/database/inc_dbconnect.php"; ?>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+</head>
+
+<script>
+	$(function (){
+		$("#hearder_frame_menu2").attr('class','hearder_frame_menu_select');
+	});
+</script>
+
+<style>
+	#wrap{width: 1000px; height: auto; margin-left: auto; margin-right: auto; background: #f8f8f8;}
+	#top_warp{width: 1000px; height: auto; margin-left: auto; margin-right: auto;}
+	#body_wrap{width: 1000px; height: auto; margin-left: auto; margin-right: auto;}
+	#bottom_wrap{width: 1000px; height: 80px; margin-left: auto; margin-right: auto; background: #f8f8f8;}
+	
+	#body_warp_frame{width: 1000px; height: auto; min-height: 500px; text-align: center; margin-left: auto; margin-right: auto; background: #f8f8f8;}
+	
+</style>
+
+
+<body bgcolor="#f0f0f0">
+	
+	<div id="wrap">
+		
+		<div id='top_warp'>
+			<?php include_once $_SERVER["DOCUMENT_ROOT"]."/landbaksa/web_code/common_top.php"; ?>
+		</div>
+		<div id="body_wrap">
+			<div id="body_warp_frame">
+				비교검색 페이지  
+			</div>
+		</div>
+		<div id="bottom_wrap">
+			
+		</div>
+	</div>	
+	
+</body>	
+</HTML>
