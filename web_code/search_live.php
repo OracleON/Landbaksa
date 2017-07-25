9fe90f03d5d394627339055fb35d651599ad2dd7
LEE SANG ON <LeeSangOn@LEEui-MacBook-Pro.local>
2017-07-25 10:49:25 +0900

    7-25-1

--- web_code/search_live.php
+++ web_code/search_live.php
@@ -10,6 +10,21 @@
 <script>
 	$(function (){
 		$("#hearder_frame_menu4").attr('class','hearder_frame_menu_select');
+		<?
+		if(isset($_COOKIE["userid"]))
+		{
+				$userid = $_COOKIE["userid"];
+				$username = $_COOKIE["username"];	
+		}
+		if($userid == null)
+		{
+		?>
+		alert('로그인이 필요합니다.\n로그인 페이지로 이동합니다.');
+		location.href = "http://pianoontest.cafe24.com/landbaksa/web_code/login.php?cate=1";
+
+		<?
+		}
+		?>
 	});
 </script>
 
