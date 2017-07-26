3d4cedabb5c776143d7d7f7304f7595b5a40c5d0
Jungmin-Hur <Jungmin5.hur@gmail.com>
2017-07-26 17:06:37 +0900

    sample code

--- web_code/CollectInfo_silprice.php
+++ web_code/CollectInfo_silprice.php
@@ -34,32 +34,50 @@
 
 	$apikey ='sGT%2FVONLJV4Uj5UsA4AM9rLhFVEF2BGSGTvprzLt5jJtkQuuzSvAWchBH5y5v4s0tADgb%2Fjl%2BTZO2Tklz2zjSg%3D%3D';
 
-// 	//
-// 	$query = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=11110&DEAL_YMD=201512&serviceKey=W3w3lbvP9c0%2FCEY0x%2Fib74UFOE3On6w%2BprL9Z1yw3GfwotJl8CvFWWpestfb4OvRabuVnoCRaAhrVqvXL4V%2B5w%3D%3D';
+	//http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=11110&DEAL_YMD=201512&serviceKey=W3w3lbvP9c0%2FCEY0x%2Fib74UFOE3On6w%2BprL9Z1yw3GfwotJl8CvFWWpestfb4OvRabuVnoCRaAhrVqvXL4V%2B5w%3D%3D
 
-	// ====> 에너지 API
-	$energy_query ='http://apis.data.go.kr/1611000/BldEngyService/getBeElctyUsgInfo?serviceKey='.$apikey.'&numOfRows=10&pageSize=10&pageNo=1&startPage=1&sigunguCd='.$sigunguCd.'&bjdongCd='.$bjdongCd.'&bun='.$bun.'&ji='.$ji.'&useYm=201601';
+	//[start] 실거래가 API
+	$silprice_url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD='.$sigunguCd.'&DEAL_YMD=201512&serviceKey='.$apikey;
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL,$silprice_url); //여기선 url을 변수로
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch,CURLOPT_NOSIGNAL, 1);
 	
-	//$response = get($energy_query); 
-	$url = $energy_query;        //호출대상 URL
-    $ch = curl_init(); //파라미터:url -선택사항
+	$data = curl_exec($ch);
+	curl_close($ch);
+	
+	$object = simplexml_load_string($data);
+	$newobject = json_encode($object);
+	$object_json = json_decode($newobject,true);
+	
+	echo("totalCount=".$object_json['body']['totalCount']."</br>");
+	echo("body=".$object_json['body']);
+// 	//[end] 실거래가 API
+// // 	$query = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=11110&DEAL_YMD=201512&serviceKey=W3w3lbvP9c0%2FCEY0x%2Fib74UFOE3On6w%2BprL9Z1yw3GfwotJl8CvFWWpestfb4OvRabuVnoCRaAhrVqvXL4V%2B5w%3D%3D';
+
+// 	// ====> 에너지 API
+// 	$energy_query ='http://apis.data.go.kr/1611000/BldEngyService/getBeElctyUsgInfo?serviceKey='.$apikey.'&numOfRows=10&pageSize=10&pageNo=1&startPage=1&sigunguCd='.$sigunguCd.'&bjdongCd='.$bjdongCd.'&bun='.$bun.'&ji='.$ji.'&useYm=201601';
+	
+// 	//$response = get($energy_query); 
+// 	$url = $energy_query;        //호출대상 URL
+//     $ch = curl_init(); //파라미터:url -선택사항
     
-    curl_setopt($ch,CURLOPT_URL,$url); //여기선 url을 변수로
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch,CURLOPT_NOSIGNAL, 1);
-    //curl_setopt($ch,CURLOPT_POST, 1); //Method를 POST로 지정.. 이 라인이 아예 없으면 GET
+//     curl_setopt($ch,CURLOPT_URL,$url); //여기선 url을 변수로
+//     curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+//     curl_setopt($ch,CURLOPT_NOSIGNAL, 1);
+//     //curl_setopt($ch,CURLOPT_POST, 1); //Method를 POST로 지정.. 이 라인이 아예 없으면 GET
     
-    $data = curl_exec($ch);
+//     $data = curl_exec($ch);
   
     
-    curl_close($ch);
+//     curl_close($ch);
     
  
-    $object = simplexml_load_string($data);
-    $newobject = json_encode($object);
-    $object_json = json_decode($newobject,true);
+//     $object = simplexml_load_string($data);
+//     $newobject = json_encode($object);
+//     $object_json = json_decode($newobject,true);
     
-    echo("totalCount=".$object_json['body']['totalCount']."</br>");
+//     echo("totalCount=".$object_json['body']['totalCount']."</br>");
 	
 	
 	 //echo($data."==>".$object."//날짜:".$resultCode."/".$useQty."/".$useYm);
@@ -86,5 +104,5 @@
 	*/	
 	
 	
-	echo $result;
+// 	echo $result;
 ?>
